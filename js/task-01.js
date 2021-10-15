0ab2c3a804ecae60cb6b467b584b95c1a1e63fc2
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2021-10-15 20:24:21 +0300

    #1

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const itemsEl = document.querySelectorAll('.item');
+console.log(`В списке ${itemsEl.length} категорий`)
+
+
+itemsEl.forEach(el => {
+    
+const h2 = el.querySelector('h2');
+    const elItems = el.querySelectorAll('li');
+    
+    console.log(
+`Категория: ${h2.textContent}
+Количество элементов: ${elItems.length}`);
+});
+
+
