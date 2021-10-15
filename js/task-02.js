7f88b584713feb053a75ce40aea3711b057940e7
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2021-10-15 20:51:45 +0300

    #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl)
+// создать отдельный ли и вписать в него массив
+const ingredientsItem = ingredients.map(el => {
+  const item = document.createElement('li');
+  item.textContent = el;
+  return item
+})
+// вставить все ли за один раз
+ingredientsEl.append(...ingredientsItem)
